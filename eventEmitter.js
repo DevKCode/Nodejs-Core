f752382a79dfbbd4fc1a1234ed34b8ee24055e63
dev <y2ksoft@gmail.com>
2022-03-09 06:10:24 +1300

    event emitter function

--- eventEmitter.js
+++ eventEmitter.js
@@ -2,15 +2,7 @@ const http = require('http');
 const EventEmitter = require('events');
 
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    console.log('started server');
-    res.write(req.url);
-res.write('Node appddd');
-res.end();
-})
 
-server.listen(8000);
 
 server.on('response', (req,res)=>{
     res.write('data received');
